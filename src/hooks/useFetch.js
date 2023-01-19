c506f27d7d1e92fb5ca198dbc8f8497fd1cf7534
PioterAndrzejewski <109315248+PioterAndrzejewski@users.noreply.github.com>
2023-01-19 13:48:24 +0100

    Handle ID changing

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,33 +1,16 @@
 import { useState, useEffect } from "react";
-import useUrl from "./useUrl";
 
 export default function useFetch() {
-	// const { url } = useUrl();
-
-	const url = "https://reqres.in/api/products?per_page=5";
-
 	const [data, setData] = useState(null);
 	const [loading, setLoading] = useState(false);
 	const [error, setError] = useState(null);
 
-	async function fetchData() {
+	async function fetchData(url) {
 		const response = await fetch(url);
 		const data = await response.json();
 		setData(data);
 		console.log(data);
 	}
 
-	useEffect(() => {
-		setLoading(true);
-		try {
-			fetchData();
-		} catch (e) {
-			setError(e);
-		} finally {
-			setLoading(false);
-			console.log(data);
-		}
-	}, [url]);
-
-	return { data, loading, error };
+	return { data, loading, error, fetchData };
 }
